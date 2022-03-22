c496ed27da947e6aa6fba929a9ac2f801ee016ca
Sam <shravanikasturi40@gmail.com>
2022-03-22 11:10:22 +0530

    first commit

---
+++ Log4jPractise/src/main/java/sravaniAcademy/Log4jPractise/App.java
@@ -0,0 +1,13 @@
+package sravaniAcademy.Log4jPractise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Log4jPractise/src/test/java/sravaniAcademy/Log4jPractise/Demo.java
@@ -0,0 +1,23 @@
+package sravaniAcademy.Log4jPractise;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+//private static Logger log=LogManager.getLogger(Demo.class.getName());
+ private static Logger	log=LogManager.getLogger(Demo.class.getName());
+	
+		@Test
+		public void getData()
+		{
+		log.debug("im debugging");	
+		log.info("object is present");
+		log.error("not present");
+		log.fatal("fatal");
+		
+	}
+	}
+
+
